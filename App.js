5da9f3b68bdee07ccd535be5c142971adf6fb6b6
emiliu <emilyliu10@gmail.com>
2018-01-20 18:20:29 -0500

    test calendar event creation and http requests

--- App.js
+++ App.js
@@ -6,11 +6,19 @@
 
 import React, { Component } from 'react';
 import {
+  Button,
   Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import * as AddCalendarEvent from 'react-native-add-calendar-event';
+
+const eventConfig = {
+  title: 'New Event',
+  startDate: '2018-01-20T08:00:00.000Z',  // 3am est
+  endDate: '2018-01-20T14:00:00.000Z'     // 9am est
+}
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -24,8 +32,31 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! Hello there
         </Text>
+        <Button
+          onPress={() => {
+
+            AddCalendarEvent.presentNewCalendarEventDialog(eventConfig)
+              .then(eventId => {
+                if (eventId) {
+                  console.warn(eventId);
+                } else {
+                  console.warn('dismissed');
+                }
+              })
+              .catch((error: string) => {
+                console.warn(error);
+              });
+
+            console.log('hello');
+
+            fetch('https://facebook.github.io/react-native/movies.json')
+              .then((response) => console.log(response));
+
+          }}
+          title="Press Me"
+        />
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
